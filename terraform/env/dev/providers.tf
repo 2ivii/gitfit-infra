c8cf19daa5c3b54925a2f0485230d3593d965473
2ivii <dbswjdals2580@gmail.com>
2025-10-30 01:52:40 +0900

    "fist init2"

---
+++ terraform/env/dev/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-2"
+  profile = "gitfit-dev"   # 아까 aws configure 한 프로필명
+}
