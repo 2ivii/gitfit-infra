a4700ea205ed616438f50070469b184296ee75eb
2ivii <dbswjdals2580@gmail.com>
2025-11-01 10:54:14 +0900

    "feat: ALB 구축"

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -9,7 +9,17 @@ module "network" {
   create_nat      = true
 }
 
+module "alb" {
+  source      = "../../modules/alb"
+  name_prefix = "gitfit-dev"
+  vpc_id      = module.network.vpc_id
+  subnets     = module.network.public_subnet_ids
+  hc_path     = "/"
+  target_port = 8080         # 나중에 ECS 컨테이너 포트와 일치시킬 것
+}
+
 output "message"            { value = "Terraform is connected to AWS successfully!" }
 output "vpc_id"             { value = module.network.vpc_id }
 output "public_subnet_ids"  { value = module.network.public_subnet_ids }
 output "private_subnet_ids" { value = module.network.private_subnet_ids }
+output "alb_dns_name" { value = module.alb.dns_name }
